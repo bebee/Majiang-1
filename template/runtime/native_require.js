2f5f4b4d0e889b682eae49f9b418d1242fdcee1c
ace.c <illj05@126.com>
2016-11-25 17:37:03 +0800

    `

--- template/runtime/native_require.js
+++ template/runtime/native_require.js
@@ -42,7 +42,6 @@ var game_file_list = [
 	"bin-debug/core/load/DialogLoding.js",
 	"bin-debug/core/PopUpManager.js",
 	"bin-debug/core/scene/GameMainScene.js",
-	"bin-debug/core/scene/LoginScene.js",
 	"bin-debug/core/SceneManager.js",
 	"bin-debug/core/StackManager.js",
 	"bin-debug/core/uicom/ChatListUI.js",
@@ -183,6 +182,7 @@ var game_file_list = [
 	"bin-debug/utils/UtilsClass/BitmapBlink.js",
 	"bin-debug/utils/UtilsClass/TipsUtils.js",
 	"bin-debug/WaitPanel.js",
+	"bin-debug/core/scene/LoginScene.js",
 	//----auto game_file_list end----
 ];
 
