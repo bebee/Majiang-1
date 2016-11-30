4899ca31bf81e6b6dafe3ed665f9da43a2f3aca9
ace.c <illj05@126.com>
2016-11-30 10:02:30 +0800

    `

--- src/component/EButton.ts
+++ src/component/EButton.ts
@@ -1,16 +1,14 @@
 /**
  * 按钮
  */
-module mui
-{
-    export class EButton extends eui.Component
-    {
+module mui {
+    export class EButton extends eui.Component {
         public textField: eui.Label;
-        private btnImg:eui.Image;
-        public textImg:eui.Image;
-        private startX:number = -1;
-        private startY:number = -1;
-        public _sounds:string = "btn_click";
+        private btnImg: eui.Image;
+        public textImg: eui.Image;
+        private startX: number = -1;
+        private startY: number = -1;
+        public _sounds: string = "btn_click";
 
         // private fun:string;
         // private thisobj:any;
@@ -22,8 +20,7 @@ module mui
          * @param tc       文字颜色
          * @param sd       文字描边规格{q:1, c:0x0} 即为强度为1，颜色为黑的描边
          */
-        public constructor(imgName: string, descStr:string = "", fontSize: number = 24, tc:number = 0xffffff, sd:any = null)
-        {
+        public constructor(imgName: string, descStr: string = "", fontSize: number = 24, tc: number = 0xffffff, sd: any = null) {
             super();
 
             this.btnImg = new eui.Image();
@@ -42,16 +39,14 @@ module mui
 
             this.textImg.touchEnabled = false;
 
-            if (descStr != "")
-            {
+            if (descStr != "") {
                 this.textField = new eui.Label();
                 this.textField.size = fontSize;
                 this.textField.textColor = tc;
                 this.textField.textAlign = "center";
                 this.textField.bold = true;
                 this.textField.fontFamily = "Microsoft YaHei";
-                if(sd)
-                {
+                if (sd) {
                     this.textField.stroke = sd["q"];
                     this.textField.strokeColor = sd["c"];
                 }
@@ -67,8 +62,9 @@ module mui
             }
 
             this.touchEnabled = true;
+            this.touchChildren = false;
 
-            this.addEventListener(egret.TouchEvent.TOUCH_END,this.onTouch, this);
+            this.addEventListener(egret.TouchEvent.TOUCH_END, this.onTouch, this);
             this.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.onTouch, this);
             this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onTouch, this);
             this.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTouch, this);
@@ -80,21 +76,19 @@ module mui
         //     this.thisobj = thisObj;
         // }
 
-        private onTouch(e:egret.TouchEvent):void
-        {
+        private onTouch(e: egret.TouchEvent): void {
             var my = this;
-            switch (e.type)
-            {
+            switch (e.type) {
                 case "touchBegin":
                     GameSound.PlaySound("" + this._sounds);
                     this.addStage();
                     my.startX = my.x;
                     my.startY = my.y;
 
-                    var tox:number = my.x + my.width * 0.025;
-                    var toy:number = my.y + my.height * 0.025;
+                    var tox: number = my.x + my.width * 0.025;
+                    var toy: number = my.y + my.height * 0.025;
 
-                    egret.Tween.get(my).to({ scaleX:0.95, scaleY:0.95, x:tox, y:toy }, 50, egret.Ease.sineIn);
+                    egret.Tween.get(my).to({scaleX: 0.95, scaleY: 0.95, x: tox, y: toy}, 50, egret.Ease.sineIn);
                     break;
                 case "touchEnd":
                     this.removeStage();
@@ -108,39 +102,32 @@ module mui
             }
         }
 
-        private onMove(e:egret.Event = null):void
-        {
-            if(this.startX == -1 && this.startY == -1) return;
+        private onMove(e: egret.Event = null): void {
+            if (this.startX == -1 && this.startY == -1) return;
 
-            if(!e || e.target != this)
-            {
-                egret.Tween.get(this).to({ scaleX: 1, scaleY: 1, x: this.startX, y: this.startY}, 50);
+            if (!e || e.target != this) {
+                egret.Tween.get(this).to({scaleX: 1, scaleY: 1, x: this.startX, y: this.startY}, 50);
 
                 //if(this.thisobj) this.thisobj["" + this.fun]();
             }
         }
 
-        private addStage():void
-        {
+        private addStage(): void {
             GameConfig.curStage().addEventListener(egret.TouchEvent.TOUCH_END, this.onMove, this);
             GameConfig.curStage().addEventListener(egret.Event.LEAVE_STAGE, this.onMove, this);
         }
 
-        private removeStage():void
-        {
+        private removeStage(): void {
             GameConfig.curStage().removeEventListener(egret.TouchEvent.TOUCH_END, this.onMove, this);
             GameConfig.curStage().removeEventListener(egret.Event.LEAVE_STAGE, this.onMove, this);
         }
 
-        public getSource()
-        {
-            if(this.btnImg) return this.btnImg.source;
+        public getSource() {
+            if (this.btnImg) return this.btnImg.source;
         }
 
-        public setSource(s:string = "")
-        {
-            if(this.btnImg) this.btnImg.source = s;
+        public setSource(s: string = "") {
+            if (this.btnImg) this.btnImg.source = s;
         }
     }
 }
-   
